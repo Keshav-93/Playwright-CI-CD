4ec140708b9b4ac8580766b84446c9db2fdf8e53
keshav-93 <keshav.g@enlume.com>
2022-07-26 12:02:43 +0530

    Running test again

--- PW/test/Firtstest.Spec.ts
+++ PW/test/Firtstest.Spec.ts
@@ -1,6 +1,7 @@
 import {test,expect} from '@playwright/test';
 
 test('Visit the Playwright url',async ({page})=>{
+	console.log("Running again");
     await page.goto('https://playwright.dev/');
     const title=page.locator('.navbar__inner .navbar__title');
     await expect(title).toHaveText('Playwright')
